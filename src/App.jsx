f9ea2ce32d77b5ea5623870547c1798c2a695ef8
Jubilio Karuna <kjubilio@gmail.com>
2026-04-27 22:54:19 +0700

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-// import AppRoutes from "./routes/AppRoutes";
-
-// function App() {
-//   return <AppRoutes />;
-// }
-
-// export default App;
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        fontFamily: "Arial",
-        textAlign: "center",
-        padding: "20px",
-      }}
-    >
-      <h1>404 Not Found.</h1>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div
+//       style={{
+//         minHeight: "100vh",
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         flexDirection: "column",
+//         fontFamily: "Arial",
+//         textAlign: "center",
+//         padding: "20px",
+//       }}
+//     >
+//       <h1>404 Not Found.</h1>
+//     </div>
+//   );
+// }
+
+// export default App;
